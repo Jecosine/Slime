748fc666f5df1aada542c55b89e5d7a6d6583109
sterben <sterben@debian.debian.com>
2018-03-18 21:21:11 -0400

    ADD:Add test filr for input management

--- check_input.py
+++ check_input.py
@@ -3,8 +3,22 @@ from get_size import get_size
 
 def close_input():
     fd = sys.stdin.fileno()
-    term_old = term_new = termios.tcgetattr(0)
-    term_new[3] &= ~termios.ECHO
-    #termios.tcsetattr(fd,termios.TCSANOW,term_old)
-    if sys.stdin.read()[-1] == '\x1b':
-        termios.tcsetattr(fd,termios.TCSANOW,term_old)
+    termIn_old = termios.tcgetattr(0)
+    termIn_new = termios.tcgetattr(0)
+    termOut_old = termios.tcgetattr(1)
+    termOut_new = termios.tcgetattr(1)
+    termIn_new[3] &= ~(termios.ECHO|termios.ICANON)
+    termOut_new[3] &= ~(termios.ECHO|termios.ICANON)
+    termios.tcsetattr(0,termios.TCSANOW,termIn_new)
+    termios.tcsetattr(1,termios.TCSANOW,termOut_new)
+    #if len(sys.stdin.read())>1:
+    #    #termios.tcsetattr(fd,termios.TCSANOW,term_old)
+    #    return "end"
+    while (sys.stdin.read(1)!="\x1b"):
+        print "233"
+    termIn_old[3] |= termios.ECHO|termios.ICANON
+    termOut_old[3] |= termios.ECHO|termios.ICANON
+    termios.tcsetattr(0,termios.TCSANOW,termIn_old);
+    termios.tcsetattr(1,termios.TCSANOW,termOut_old);
+
+close_input ()
