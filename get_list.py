efe30ffdb146f2f7f6f40c4bbbd1afe67e4e44a3
sterben <mj_ol@outlook.com>
2018-03-23 01:01:15 -0400

    UPDATE&ADD:Fix problem about saving the position;Add script to process the saved string and convert it into list

---
+++ get_list.py
@@ -0,0 +1,17 @@
+l = []
+f = open("saved",'rb')
+content = f.read()
+content = content[1:-1]
+elements = content.split('], [')
+temp = ""
+
+for element in elements:
+    c = element.split(',')
+    if c[0][0] == '[':
+        c[0] = c[0][1:]
+    if c[1][-1] == ']':
+        c[1] = c[1][:-1]
+    l.append([int(c[0]),int(c[1])])
+    print c
+print l
+  
