d14f51913f62940d6e4d38fffc9e2f91973a4f1e
sterben <mj_ol@outlook.com>
2018-03-28 01:31:50 -0400

    UPDATE:Give some tasty food to snake

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+import random
 import sys
 from get_list import get_list
 import Base_Input as bi
@@ -5,6 +6,8 @@ from GameFlow import Game
 import GameObject as go
 from get_size import get_size
 import Engine_Utils as utils
+import time
+
 class Snake(go.Object):
     def __init__(self,name):
         go.Object.__init__(self,name)
@@ -29,33 +32,55 @@ class SnakeGame(Game):
     def __init__(self):
         snake = Snake("snake")
         global snake
+        food = go.Object("food")
+        global food
         Game.__init__(self)
         #self.gameover = False
         self.direction = [-1,0]
         self.add_object(snake)
+        self.add_object(food)
         self.isPause = False
         self.frame = 0.1 
+        self.foodpos = []
+        self.instance_food()
+    def instance_food(self):
+        food.pixels = []
+        random.seed(int(time.time()))
+        temppos = [random.randrange(self.cols+1),random.randrange(self.rows)]
+        if self.foodpos == temppos:
+            self.foodpos = instance_food()
+        else:
+            self.foodpos = temppos
+        food.add_pixel([go.pixel(self.foodpos)])
+        self.iseaten = False
+        return 0
+
     def get_command(self,s):
         if s == ":q":
             self.isrunning = False
         if s == ":r":
             self.__init__()
     def Move(self,c):
-        if c == "w":
+        if c == "w" and self.direction<>[0,-1]:
             self.direction = [0,1]
-        if c == "s":
+        if c == "s" and self.direction<>[0,1]:
             self.direction = [0,-1]
-        if c == "d":
+        if c == "d" and self.direction<>[-1,0]:
             self.direction = [1,0]
-        if c == "a":
+        if c == "a" and self.direction<>[1,0]:
             self.direction = [-1,0]
         if c == "\x1b":
             self.set_pause()
             return 0
         snake.position = utils.vector_add(snake.position,self.direction)
+        temp,snake.position = self.transform(snake.position) 
         if snake.judge(snake.position):
-            snake.add_p(snake.position)
-            snake.pixels.pop(-1)
+            if snake.position == self.foodpos:
+                snake.add_p(snake.position)
+                self.iseaten = True
+            else:
+                snake.add_p(snake.position)
+                snake.pixels.pop(-1)
         else:
             self.gameover()
         return 0
@@ -71,17 +96,39 @@ class SnakeGame(Game):
         self.render_once()
         self.set_pause()
         return 0
+    def fill_panel(self):
+        self.canva = []
+        if len(self.objects)<>0:
+            for obj in self.objects:
+                for p in obj.pixels:
+                    temp, p.position = self.transform(p.position)
+                    if temp not in self.canva and (temp[0]<=self.rows-1):
+                        self.canva.append(temp)
+        return self.canva
 
+    def transform(self,pos):
+        trans = [self.rows-pos[1],pos[0]]
+        if trans[0] <= 0:
+            trans[0] = self.rows + trans[0]
+        if trans[1] <= 0:
+            trans[1] = self.cols + trans[1]
+        if trans[0] > self.rows:
+            trans[0] = trans[0] - self.rows
+        if trans[1] > self.cols:
+            trans[1] = trans[1] - self.cols
+        return trans, [trans[1],self.rows - trans[0]]
     def update(self):
         s = ''
         self.isrunning = True
+        self.iseaten = False
         while(self.isrunning):
-
+            if self.iseaten:
+                self.instance_food()
             c = bi.get_input()
             s = c
             self.canva = self.fill_panel()
-            self.isrunning = True
-            self.current_log = str(len(snake.pixels))
+            #self.isrunning = True
+            self.current_log = str([snake.pixels[i].position for i in range(len(snake.pixels))]) + str(self.canva)
             self.update_log()
             if not self.isPause:
                 self.Move(s)
